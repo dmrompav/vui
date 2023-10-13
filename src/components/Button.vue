c9d440c5fb18704692b3b6adfa7677cbc6e29ada
dmrompav <dm.rom.pav@gmail.com>
2023-10-13 12:17:59 +0600

    first init

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot>
+      <span>{{ props.label }}</span>
+    </slot>
+  </button>
+</template>
+
+<script setup lang='ts'>
+const props = withDefaults(defineProps<{
+  label: string | number
+}>(), {
+
+});
+</script>
